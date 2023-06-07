ed07a30a1d1adb1357d394570ff3f17595e57323
Cameron Kirkpatrick <cameronk@appstrax.tech>
2023-06-07 13:07:22 +0200

    Added day_8

---
+++ week_2/day_8/task-list/src/components/TaskTable.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default function TaskTable(props) {
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Complete</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.tasks.map((task) => {
+            return (
+              <tr key={task.id}>
+                <td>{task.name}</td>
+                <td>
+                  {/* arrow function used because we want access to variable inside component */}
+                  <div onClick={() => props.onTaskCompleteToggle(task.id)}>
+                    {/* non arrow function used when we want the event as the parameter */}
+                    {/* <div onClick={props.onTaskCompleteToggle}> */}
+                    <i
+                      className={
+                        task.complete ? 'bi bi-circle-fill' : 'bi bi-circle'
+                      }
+                    ></i>
+                  </div>
+                </td>
+                <td>
+                  <div onClick={() => props.onTaskRemove(task.id)}>
+                    <i className="bi bi-trash"></i>
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
